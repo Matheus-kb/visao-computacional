2e931d74d8b2bee4dbf520d29d99ba2475a322e8
Matheus-kb <matheuskbitencourt@gmail.com>
2024-07-18 16:49:13 -0300

    feat: Carregando o classificador de faces

--- analise-de-sentimento-facial/main.py
+++ analise-de-sentimento-facial/main.py
@@ -6,3 +6,7 @@ import numpy as np
 # Carregar o modelo Emotion do DeepFace
 model = DeepFace.build_model("Emotion")
 emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+
+# Carregar o classificador de faces
+face_cascade = cv2.CascadeClassifier('c:\\Projetos\\visao-computacional\\analise-de-sentimento-facial\\haarcascade_frontalface_alt2.xml')
+cap = cv2.VideoCapture(0)  # Para usar a câmera
